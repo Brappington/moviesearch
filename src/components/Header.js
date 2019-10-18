f4aebdf892ed1dd125f8b67675b21d44487e07a8
brap <doctor.fun107@gmail.com>
2019-10-18 14:21:39 +0100

    experimenting with link back to jackpowell.dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 const Header = (props) => {
     return (
         <header className="App-header">
+            <h3 class="name"><a href="https://jackpowell.dev">Jack Powell</a></h3>
             <h2>{props.text}</h2>
         </header>
     );
